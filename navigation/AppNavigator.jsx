76ace420cf00b2a94937e8b256138a3afeda00f8
JD Browne <jdbrowne01@gmail.com>
2021-12-06 10:47:14 -0700

    Added drawer navigation to ShopNavigator and updated AppNavigator to use drawer navigator as primary

--- navigation/AppNavigator.jsx
+++ navigation/AppNavigator.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { ProductsNavigator } from './ShopNavigator'
+import { ShopNavigator } from './ShopNavigator'
 import { NavigationContainer } from '@react-navigation/native';
 
 const AppNavigator = props => {
     return (
         <NavigationContainer>
-            <ProductsNavigator />
+            <ShopNavigator />
         </NavigationContainer>
     )
 }
--- navigation/ShopNavigator.jsx
+++ navigation/ShopNavigator.jsx
@@ -1,12 +1,35 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import { Platform } from "react-native";
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+} from "@react-navigation/drawer";
+import {
+  Platform,
+  SafeAreaView,
+  Button,
+  View,
+  StyleSheet,
+  Text,
+} from "react-native";
+import { useDispatch } from "react-redux";
+import { Ionicons } from "@expo/vector-icons";
 import ProductOverviewScreen, {
   screenOptions as ProductOverviewScreenOptions,
 } from "../screens/shop/ProductOverviewScreen";
 import Colors from "../constants/Colors";
-import ProductDetailsScreen, {screenOptions as ProductDetailsScreenOptions } from '../screens/shop/ProductDetailsScreen'
-import CartScreen, {screenOptions as CartScreenOptions} from "../screens/shop/CartScreen";
+import ProductDetailsScreen, {
+  screenOptions as ProductDetailsScreenOptions,
+} from "../screens/shop/ProductDetailsScreen";
+import CartScreen, {
+  screenOptions as CartScreenOptions,
+} from "../screens/shop/CartScreen";
+import OrdersScreen, {
+  screenOptions as OrdersScreenOptions,
+} from "../screens/shop/OrdersScreen";
+import { NavigationContainer } from "@react-navigation/native";
 
 //Set up default nav options
 const defaultNavOptions = {
@@ -14,10 +37,10 @@ const defaultNavOptions = {
     backgroundColor: Platform.OS === "android" ? Colors.primary : "",
   },
   headerTitleStyle: {
-    fontFamily: 'open-sans-bold'
+    fontFamily: "open-sans-bold",
   },
   headerBackTitleStyle: {
-    fontFamily: 'open-sans'
+    fontFamily: "open-sans",
   },
   headerTintColor: Platform.OS === "android" ? "white" : Colors.primary,
 };
@@ -33,16 +56,118 @@ export const ProductsNavigator = () => {
         component={ProductOverviewScreen}
         options={ProductOverviewScreenOptions}
       />
-    <ProductsStackNavigator.Screen 
-      name="ProductDetail"
-      component={ProductDetailsScreen}
-      options={ProductDetailsScreenOptions}
-    />
-    <ProductsStackNavigator.Screen
-    name="Cart"
-    component={CartScreen}
-    options={CartScreenOptions}
-    />
+      <ProductsStackNavigator.Screen
+        name="ProductDetail"
+        component={ProductDetailsScreen}
+        options={ProductDetailsScreenOptions}
+      />
+      <ProductsStackNavigator.Screen
+        name="Cart"
+        component={CartScreen}
+        options={CartScreenOptions}
+      />
     </ProductsStackNavigator.Navigator>
   );
 };
+
+// Set up a different stack navigator for orders
+
+const OrdersStackNavigator = createStackNavigator();
+
+export const OrdersNavigator = () => {
+  return (
+    <OrdersStackNavigator.Navigator screenOptions={defaultNavOptions}>
+      <OrdersStackNavigator.Screen
+        name="Orders"
+        component={OrdersScreen}
+        options={OrdersScreenOptions}
+      />
+    </OrdersStackNavigator.Navigator>
+  );
+};
+
+// Set up for the drawer navigator - here we can use the stack navigators to create the options in the drawer
+const ShopDrawerNavigator = createDrawerNavigator();
+
+export const ShopNavigator = () => {
+  const dispatch = useDispatch();
+
+  return (
+    <ShopDrawerNavigator.Navigator
+      drawerContent={(props) => {
+        return (
+          <View style={{ flex: 1, paddingTop: 20 }}>
+            <SafeAreaView forceInset={{ top: "always", horizontal: "never" }}>
+              <DrawerItemList {...props} />
+              <Button
+                title="Logout"
+                color={Colors.primary}
+                onPress={() => {}}
+              />
+            </SafeAreaView>
+          </View>
+        );
+      }}
+      screenOptions={{
+        activeTintColor: Colors.primary,
+        drawerActiveTintColor: Colors.primary,
+        drawerStyle: {
+          paddingTop: "20%", // Pushes all items in drawer down
+        },
+        drawerItemStyle: {
+          paddingTop: 10,
+        },
+        drawerInactiveTintColor: "#808080",
+        headerShown: false,
+      }}
+    >
+      <ShopDrawerNavigator.Screen
+        name="All Products"
+        component={ProductsNavigator}
+        screenOptions={{}}
+        options={({ navigation }) => ({
+          drawerIcon: (props) => (
+            <Ionicons
+              name={Platform.OS === "android" ? "md-cart" : "ios-cart"}
+              size={23}
+              color={props.color}
+            />
+          ),
+        })}
+      />
+      <ShopDrawerNavigator.Screen
+        name="Your Orders"
+        component={OrdersNavigator}
+        options={{
+          drawerIcon: (props) => (
+            <Ionicons
+              name={Platform.OS === "android" ? "md-list" : "ios-list"}
+              size={23}
+              color={props.color}
+            />
+          ),
+        }}
+      />
+    </ShopDrawerNavigator.Navigator>
+  );
+};
+
+// options={({ navigation }) => ({ //This is an example to use navigation in the default headers
+
+//title: 'All Products',
+// backgroundColor: Colors.primary,
+// headerTitle: () => (
+//   <Text style={styles.headerTitle}>All Products</Text>
+// ),
+// headerRight: () => (
+//   <View style={styles.headerRight}>
+//     <Ionicons
+//       name={Platform.OS === "android" ? "md-cart" : "ios-cart"}
+//       size={23}
+//       color={Platform.OS === "android" ? "white" : Colors.primary}
+//       onPress={() => {
+//         navigation.navigate("Cart");
+//       }}
+//     />
+//   </View>
+// ),
