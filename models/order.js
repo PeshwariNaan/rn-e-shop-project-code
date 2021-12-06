351e1d2e7cfe781dde2b1a31543bffad687db10d
JD Browne <jdbrowne01@gmail.com>
2021-12-06 11:40:00 -0700

    Created OrderItem to output the orders. Changed order model to be able to output date using moment module

--- models/order.js
+++ models/order.js
@@ -1,5 +1,5 @@
 // This is the structure of the orders we will be storing for each customer
-
+ import moment from "moment"
 class Order {
     constructor(id, items, totalAmount, date) {
         this.id = id  // Different than the product id - this is specific to orders
@@ -7,5 +7,17 @@ class Order {
         this.totalAmount = totalAmount
         this.date = date
     }
+
+    get readableDate() {
+        // return this.date.toLocaleDateString('en-EN', { //Android does not support this so we use the moment module: npm install --save moment
+        //     year: 'numeric',
+        //     month: 'long',
+        //     day: 'numeric',
+        //     hour: '2-digit',
+        //     minute: '2-digit'
+        // })
+     return moment(this.date).format('MMMM Do YYYY, hh:mm');
+}
+
 }
 export default Order
