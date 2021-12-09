9ac2788134719b637009b3c07241a59007f36c68
JD Browne <jdbrowne01@gmail.com>
2021-12-09 16:17:52 -0700

    Added ability to pull to refresh on the products overview screen

--- screens/shop/ProductOverviewScreen.jsx
+++ screens/shop/ProductOverviewScreen.jsx
@@ -10,12 +10,11 @@ import {
 } from "react-native";
 import ProductItem from "../../components/shop/ProductItem";
 import { useSelector, useDispatch } from "react-redux";
-import { Ionicons } from "@expo/vector-icons";
 import Colors from "../../constants/Colors";
-import * as CartActions from '../../store/actions/cartActions'
+import * as CartActions from "../../store/actions/cartActions";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import HeaderButton from "../../components/UI/HeaderButton";
-import * as productActions from '../../store/actions/productActions'
+import * as productActions from "../../store/actions/productActions";
 
 const ProductOverviewScreen = (props) => {
   const [isLoading, setIsLoading] = useState(false);
@@ -24,12 +23,13 @@ const ProductOverviewScreen = (props) => {
 
   //Allows us to tap into the state to get access to the availableProducts
   const products = useSelector((state) => state.products.availableProducts);
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   //This useEffect call is so when we navigate away from this page and come back - The page will reload
   useEffect(() => {
-    const unsubscribe = props.navigation.addListener('focus', loadProducts);
-    return () => {// This is a clean-up function - we clean up the listener
+    const unsubscribe = props.navigation.addListener("focus", loadProducts);
+    return () => {
+      // This is a clean-up function - we clean up the listener
       unsubscribe();
     };
   }, [loadProducts]);
@@ -46,113 +46,116 @@ const ProductOverviewScreen = (props) => {
     setIsRefreshing(false);
   }, [dispatch, setIsLoading, setError]);
 
-// We need this useEffect call to initally load products
-useEffect(() => {
-  setIsLoading(true)
-  loadProducts().then(() => {
-    setIsLoading(false)
-  })
-},[dispatch, loadProducts])
-
+  // We need this useEffect call to initally load products
+  useEffect(() => {
+    setIsLoading(true);
+    loadProducts().then(() => {
+      setIsLoading(false);
+    });
+  }, [dispatch, loadProducts]);
 
   // Function: Receives product id and title to navigate to particular items detail page
-const selectItemHandler = (id, title) => {
-  props.navigation.navigate('ProductDetail', {
-    productId: id,
-    productTitle: title // These identifiers are used in the Details screen - take note of the syntax as they are used to understand how react nav 6x works
-  })
-}
+  const selectItemHandler = (id, title) => {
+    props.navigation.navigate("ProductDetail", {
+      productId: id,
+      productTitle: title, // These identifiers are used in the Details screen - take note of the syntax as they are used to understand how react nav 6x works
+    });
+  };
 
-if (error) {
-  return (
-    <View style={styles.centered}>
-      <Text>An error occurred!</Text>
-      <Button
-        title="Try again"
-        onPress={loadProducts}
-        color={Colors.primary}
-      />
-    </View>
-  );
-}
+  if (error) {
+    return (
+      <View style={styles.centered}>
+        <Text>An error occurred!</Text>
+        <Button
+          title="Try again"
+          onPress={loadProducts}
+          color={Colors.primary}
+        />
+      </View>
+    );
+  }
 
-if (isLoading) {
-  return (
-    <View style={styles.centered}>
-      <ActivityIndicator size="large" color={Colors.primary} />
-    </View>
-  );
-}
+  if (isLoading) {
+    return (
+      <View style={styles.centered}>
+        <ActivityIndicator size="large" color={Colors.primary} />
+      </View>
+    );
+  }
 
-if (!isLoading && products.length === 0) {
-  return (
-    <View style={styles.centered}>
-      <Text>No products found. Maybe start adding some!</Text>
-    </View>
-  );
-}
-
+  if (!isLoading && products.length === 0) {
+    return (
+      <View style={styles.centered}>
+        <Text>No products found. Maybe start adding some!</Text>
+      </View>
+    );
+  }
 
   return (
     <FlatList
+      onRefresh={loadProducts}
+      refreshing={isRefreshing}
       data={products}
       // keyExtractor={item => item.id} // ** Later versions of react-native no longer need keyExtractor **
-      renderItem={(itemData) => 
-      (
+      renderItem={(itemData) => (
         <ProductItem
-        image={itemData.item.imageUrl}
-        title={itemData.item.title}
-        price={itemData.item.price}
-        onSelect={() => {selectItemHandler(itemData.item.id, itemData.item.title)}}
+          image={itemData.item.imageUrl}
+          title={itemData.item.title}
+          price={itemData.item.price}
+          onSelect={() => {
+            selectItemHandler(itemData.item.id, itemData.item.title);
+          }}
         >
-        <Button
-        color={Colors.primary}
-        title="View Details"
-        onPress={() => {selectItemHandler(itemData.item.id, itemData.item.title)}}
-         />
-         <Button
-        color={Colors.primary}
-        title="To Cart"
-        onPress={() => {dispatch(CartActions.addToCart(itemData.item))}}
-         />
-
+          <Button
+            color={Colors.primary}
+            title="View Details"
+            onPress={() => {
+              selectItemHandler(itemData.item.id, itemData.item.title);
+            }}
+          />
+          <Button
+            color={Colors.primary}
+            title="To Cart"
+            onPress={() => {
+              dispatch(CartActions.addToCart(itemData.item));
+            }}
+          />
         </ProductItem>
       )}
     />
-    
   );
 };
 
-export const screenOptions = navData => {
-    return {
-        headerTitle: 'All Products',
-        headerLeft: () => (
-          <HeaderButtons HeaderButtonComponent={HeaderButton}>
-            <Item
-            title='Menu'
-            iconName={Platform.OS === 'android' ? 'md-menu' : 'ios-menu'}
-            onPress={() => {
-              navData.navigation.toggleDrawer();
-            }}
-            />
-          </HeaderButtons>
-        ),
-        headerRight: () => (
-          <HeaderButtons HeaderButtonComponent={HeaderButton}>
-            <Item
-              title="Cart"
-              iconName={Platform.OS === 'android' ? 'md-cart' : 'ios-cart'}
-              onPress={() => {
-                navData.navigation.navigate('Cart');
-              }}
-            />
-          </HeaderButtons>
-        )
-    }
-}
+export const screenOptions = (navData) => {
+  return {
+    headerTitle: "All Products",
+    headerLeft: () => (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Menu"
+          iconName={Platform.OS === "android" ? "md-menu" : "ios-menu"}
+          onPress={() => {
+            navData.navigation.toggleDrawer();
+          }}
+        />
+      </HeaderButtons>
+    ),
+    headerRight: () => (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Cart"
+          iconName={Platform.OS === "android" ? "md-cart" : "ios-cart"}
+          onPress={() => {
+            navData.navigation.navigate("Cart");
+          }}
+        />
+      </HeaderButtons>
+    ),
+  };
+};
 
 export default ProductOverviewScreen;
 
 const styles = StyleSheet.create({
-  centered: { flex: 1, justifyContent: 'center', alignItems: 'center' }
+  centered: { flex: 1, justifyContent: "center", alignItems: "center" },
 });
