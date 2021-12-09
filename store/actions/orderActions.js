ae910f9c790574121a21bfe5eefa968bcb9f4c43
JD Browne <jdbrowne01@gmail.com>
2021-12-09 15:35:29 -0700

    Added functionality for storing orders on Firebase in add order action

--- store/actions/orderActions.js
+++ store/actions/orderActions.js
@@ -1,8 +1,35 @@
 export const ADD_ORDER = 'ADD_ORDER';
 
 export const addOrder = (cartItems, totalAmount) => {
-    return {
+    return async (dispatch, getState) => {     
+      const date = new Date();
+      const response = await fetch(
+        `https://react-native-shop-f6bef-default-rtdb.firebaseio.com/orders/u1.json`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            cartItems,
+            totalAmount,
+            date: date.toISOString()
+          })
+        }
+      );  
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }  
+      const resData = await response.json();
+        
+      dispatch({
         type: ADD_ORDER,
-        orderData: {items: cartItems, amount: totalAmount}
-    }
-}
+        orderData: {
+          id: resData.name,
+          items: cartItems,
+          amount: totalAmount,
+          date: date
+        }
+      });
+    };
+  };
--- store/reducers/orderReducer.js
+++ store/reducers/orderReducer.js
@@ -9,10 +9,10 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case orderActions.ADD_ORDER:
       const newOrder = new Order(
-        new Date().toString(),  // temp id before we implement the firebase db
+        action.orderData.id,
         action.orderData.items,
         action.orderData.amount,
-        new Date()
+        action.orderData.date
       );
       return {
         ...state,
